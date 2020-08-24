dc3667b3892d5d7256430c1e8bc7b462bb01a68f
Alla Barkat <a2la1101@gmail.com>
2020-08-24 22:02:26 +0200

    Building The basic building blocks

---
+++ src/AllaGeneratorServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace alla\Generator;
+
+use Illuminate\Support\ServiceProvider;
+use alla\Generator\Commands\GeneratorBuilderCommand;
+
+class AllaGeneratorServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands(
+                [
+                    GeneratorBuilderCommand::class,
+                ]
+            );
+        }
+    }
+}
---
+++ src/Commands/GeneratorBuilderCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace alla\Generator\Commands;
+
+use Illuminate\Console\Command;
+
+class GeneratorbuilderCommand extends Command
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'alla:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create And Build Services And Boards';
+
+
+    public function handle()
+    {
+
+    }
+}
